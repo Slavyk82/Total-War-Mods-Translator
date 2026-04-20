2e42e66ee2b65aa2ee52a56eaa2d1b00990b4948
Slavyk <j.pennegues@gmail.com>
2026-04-21 00:20:28 +0200

    fix: use structural equality in numbers validation rule

--- lib/services/translation/validation_service_impl.dart
+++ lib/services/translation/validation_service_impl.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:twmt/models/common/result.dart';
 import 'package:twmt/models/common/validation_issue_entry.dart';
 import 'package:twmt/models/common/validation_result.dart' as common;
@@ -550,7 +551,8 @@ class ValidationServiceImpl implements IValidationService {
     // Check for number mismatches
     final sourceNumbers = TextParserUtils.extractNumbers(sourceText);
     final translatedNumbers = TextParserUtils.extractNumbers(translatedText);
-    if (sourceNumbers.isNotEmpty && sourceNumbers != translatedNumbers) {
+    if (sourceNumbers.isNotEmpty &&
+        !listEquals(sourceNumbers, translatedNumbers)) {
       mistakes.add(ValidationError(
         rule: ValidationRule.numbers,
         severity: ValidationSeverity.warning,
